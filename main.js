091ff21dfb026b29aba07fea9183d43ebc8eaced
Abhishek Ranjan <rabhishekranjan@gmail.com>
2022-07-27 15:27:17 +0530

    impliment draw on canvas

--- main.js
+++ main.js
@@ -1,22 +1,37 @@
 let container = document.querySelector(".canvas");
+let val = document.querySelector("#size");
+let gridBoxes;
 
-function makeRows(rows, cols) {
+let mouseDown = false;
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
+
+function makeGrids(rows, cols) {
+  container.innerHTML = "";
   console.log("exp", rows, cols);
   container.style.setProperty("--grid-rows", rows);
   container.style.setProperty("--grid-cols", cols);
 
   for (c = 0; c < rows * cols; c++) {
     let cell = document.createElement("div");
-    // cell.innerText = c + 1;
-    container.appendChild(cell).className = "grid-item";
+
+    cell.addEventListener("mouseover", changeColor);
+    cell.addEventListener("mousedown", changeColor);
+    container.appendChild(cell).classList.add("grid-item");
   }
+  // gridBoxes = document.querySelectorAll(".grid-item");
+  // gridBoxes.forEach((element) => {
+  //   element.onmousemove = (e) => e.target.classList.add("grid-item-color");
+  // });
 }
 
-console.log("what is wrong");
-let val = document.querySelector("#size");
-console.dir(val);
+function changeColor(e) {
+  if (e.type === "mouseover" && !mouseDown) return;
+  e.target.classList.add("grid-item-color");
+}
+
+makeGrids(10, 10);
+
 val.onchange = (e) => {
-  container.style.setProperty("--grid-rows", 0);
-  container.style.setProperty("--grid-cols", 0);
-  makeRows(e.target.value, e.target.value);
+  makeGrids(e.target.value, e.target.value);
 };
